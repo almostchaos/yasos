d4d907c066b9b768a211541205823c3747ac3853
Mircea Toma <mircea.toma@icesoft.com>
2023-09-29 17:26:16 +0300

    Refactor macro to avoid using atom.

--- src/yasos/object.clj
+++ src/yasos/object.clj
@@ -1,23 +1,18 @@
 (ns yasos.object)
 
-(defn define-object [define-method]
-  (let [methods (atom {})]
-    (define-method (fn [op meth]
-                     (swap! methods assoc op meth)))
-    (fn self [op args]
-      (let [meth (get @methods op)]
-        (if (nil? meth)
-          (throw (NoSuchMethodException. (str op)))
-          (apply meth args))))))
-
 (defmacro operator [name]
   `(defn ~name [~'object & ~'args]
      (~'object ~name ~'args)))
 
 (defmacro object [& body]
-  `(define-object (fn [~'add-method] ~@body)))
+  `(let [~'methods (into {} [~@body])]
+     (fn [~'op ~'args]
+       (let [~'meth (get ~'methods ~'op)]
+         (if (nil? ~'meth)
+           (throw (NoSuchMethodException. (str ~'op)))
+           (apply ~'meth ~'args))))))
 
 (defmacro method [op & body]
   (if (vector? (first body))
-    `(~'add-method ~op (fn ~(first body) ~@(rest body)))
-    `(~'add-method ~op (fn ~@body))))
+    `[~op (fn ~(first body) ~@(rest body))]
+    `[~op (fn ~@body)]))
